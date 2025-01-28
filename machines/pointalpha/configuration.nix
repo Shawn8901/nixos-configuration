90c0d279da03d441a6d6ba6da755ef72f4ffc069
Shawn8901 <shawn8901@googlemail.com>
2025-01-28 20:22:54 +0100

    pointalpha: pipewire config

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -155,6 +155,49 @@ in
       enable = true;
       devices = [ { device = "/dev/nvme1"; } ];
     };
+    pipewire = {
+      wireplumber.extraConfig = {
+        "10-bluez" = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+          };
+          "11-bluetooth-policy" = {
+            "wireplumber.settings" = {
+              "bluetooth.autoswitch-to-headset-profile" = false;
+            };
+          };
+          "92-low-latency" = {
+            "context.properties" = {
+              "default.clock.rate" = 48000;
+              "default.clock.quantum" = 32;
+              "default.clock.min-quantum" = 32;
+              "default.clock.max-quantum" = 32;
+            };
+          };
+        };
+      };
+      extraConfig.pipewire-pulse."92-low-latency" = {
+        "context.properties" = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = { };
+          }
+        ];
+        "pulse.properties" = {
+          "pulse.min.req" = "32/48000";
+          "pulse.default.req" = "32/48000";
+          "pulse.max.req" = "32/48000";
+          "pulse.min.quantum" = "32/48000";
+          "pulse.max.quantum" = "32/48000";
+        };
+        "stream.properties" = {
+          "node.latency" = "32/48000";
+          "resample.quality" = 1;
+        };
+      };
+    };
   };
 
   hardware = {
