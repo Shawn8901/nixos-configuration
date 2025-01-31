797697834fdd0b90d3c5689dba88356da2104aaa
Shawn8901 <shawn8901@googlemail.com>
2025-02-01 00:13:08 +0100

    managed-user: use starship instead p10k

--- modules/nixos/private/managed-user.nix
+++ modules/nixos/private/managed-user.nix
@@ -35,6 +35,56 @@ in
         fuzzyCompletion = true;
         keybindings = true;
       };
+      starship = {
+        enable = true;
+        interactiveOnly = true;
+        settings = {
+          command_timeout = 2000;
+          # Don"t print a new line at the start of the prompt
+          add_newline = false;
+
+          # Wait 10 milliseconds for starship to check files under the current directory.
+          scan_timeout = 10;
+
+          directory = {
+            truncation_length = 3;
+            truncation_symbol = "…";
+          };
+
+          #to display the hostname before the character line
+          hostname = {
+            ssh_only = false;
+            style = "blue";
+            format = "[$hostname]($style) in ";
+            disabled = false;
+          };
+          #the character at the start of line where command is entered
+          character = {
+            error_symbol = "[✗](bold red)";
+            vicmd_symbol = "[V](bold green)";
+          };
+
+          git_branch = {
+            symbol = "🌿 ";
+          };
+          git_commit = {
+            disabled = false;
+          };
+
+          git_status = {
+            ahead = "⇡ $count";
+            diverged = "⇕ ⇡ $ahead_count ⇣ $behind_count";
+            behind = "⇣ $count";
+          };
+          memory_usage = {
+            format = "$symbol[$ram( | $swap)]($style) ";
+            symbol = "🌒️";
+            threshold = 50;
+            style = "bold dimmed white";
+            disabled = false;
+          };
+        };
+      };
       zsh = {
         enable = true;
         enableCompletion = true;
@@ -42,11 +92,6 @@ in
         enableGlobalCompInit = true;
         syntaxHighlighting.enable = true;
         autosuggestions.enable = true;
-        promptInit = ''
-          source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
-          source "${../../../files/p10k.zsh}"
-
-        '';
         interactiveShellInit = ''
           source "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
 
