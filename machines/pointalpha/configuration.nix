65efbc0622dac6102c1d07ae662f48c5d775e7fa
Shawn8901 <shawn8901@googlemail.com>
2025-05-02 19:27:47 +0200

    desktop: set rt settings via pam

--- machines/pointalpha/configuration.nix
+++ machines/pointalpha/configuration.nix
@@ -152,45 +152,12 @@ in
     };
     pipewire = {
       wireplumber.extraConfig = {
-        "10-bluez" = {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-          };
-          "11-bluetooth-policy" = {
-            "wireplumber.settings" = {
-              "bluetooth.autoswitch-to-headset-profile" = false;
-            };
-          };
-          "92-low-latency" = {
-            "context.properties" = {
-              "default.clock.rate" = 48000;
-              "default.clock.quantum" = 32;
-              "default.clock.min-quantum" = 32;
-              "default.clock.max-quantum" = 32;
-            };
-          };
+        "10-bluez"."monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
         };
-      };
-      extraConfig.pipewire-pulse."92-low-latency" = {
-        "context.properties" = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = { };
-          }
-        ];
-        "pulse.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "32/48000";
-          "pulse.max.req" = "32/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "32/48000";
-        };
-        "stream.properties" = {
-          "node.latency" = "32/48000";
-          "resample.quality" = 1;
-        };
+        "11-bluetooth-policy"."wireplumber.settings"."bluetooth.autoswitch-to-headset-profile" = false;
       };
     };
   };
--- modules/nixos/private/desktop.nix
+++ modules/nixos/private/desktop.nix
@@ -85,6 +85,28 @@ in
       rtkit.enable = true;
       auditd.enable = false;
       audit.enable = false;
+      # Upstream pipewire limits for realtime
+      # https://gitlab.freedesktop.org/pipewire/pipewire/-/blob/master/meson_options.txt#L342
+      pam.loginLimits = [
+        {
+          domain = "@users";
+          item = "rtprio";
+          type = "-";
+          value = "95";
+        }
+        {
+          domain = "@users";
+          item = "memlock";
+          type = "-";
+          value = "4194304";
+        }
+        {
+          domain = "@users";
+          item = "nice";
+          type = "-";
+          value = "-19";
+        }
+      ];
     };
 
     systemd.defaultUnit = "graphical.target";
