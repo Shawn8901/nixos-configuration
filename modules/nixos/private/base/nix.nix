17f1fa4c3e24d1264c493d206df1b7a8194e19b4
Shawn8901 <shawn8901@googlemail.com>
2025-08-16 13:29:23 +0200

    base: keep more generations

--- modules/nixos/private/base/nix.nix
+++ modules/nixos/private/base/nix.nix
@@ -63,7 +63,7 @@ in
     flake = lib.mkDefault "github:shawn8901/nixos-configuration";
     clean = {
       enable = true;
-      extraArgs = "--keep 3 --keep-since 3d";
+      extraArgs = "--keep 5 --keep-since 14d";
     };
   };
 }
