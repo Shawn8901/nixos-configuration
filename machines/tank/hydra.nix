827b0c6be7158550ae36236528b3212aa7071582
Shawn8901 <shawn8901@googlemail.com>
2025-08-27 20:19:19 +0200

    tank: dont build aarch64

--- machines/tank/hydra.nix
+++ machines/tank/hydra.nix
@@ -60,8 +60,6 @@ in
       ];
     };
 
-    boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
     systemd.tmpfiles.rules = [ "f /tmp/hyda/dynamic-machines 666 hydra hydra - " ];
 
     services = {
@@ -237,7 +235,6 @@ in
             systems = [
               "x86_64-linux"
               "i686-linux"
-              "aarch64-linux"
             ];
             supportedFeatures = supportedFeatures ++ [ "gccarch-x86-64-v3" ];
             inherit maxJobs;
