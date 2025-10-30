206b2985e7751e23f88601bef7e8c18a6fab6c21
Shawn8901 <shawn8901@googlemail.com>
2025-10-30 19:55:18 +0100

    tank/hydra: use pkgs.hydra

--- machines/tank/hydra.nix
+++ machines/tank/hydra.nix
@@ -130,7 +130,7 @@ in
           enable = true;
           listenHost = "127.0.0.1";
           port = 3000;
-          package = pkgs.hydra_unstable;
+          package = pkgs.hydra;
           notificationSender = cfg.mailAdress;
           buildMachinesFiles = [
             "/etc/nix/machines"
