e404fa6184c10b17b5731259c4538a97ffc0ca33
Shawn8901 <shawn8901@googlemail.com>
2026-03-31 22:57:55 +0200

    zrepl-admin: automatically rotate new keys

--- modules/aspects/cfg/zrepl-admin/default.nix
+++ modules/aspects/cfg/zrepl-admin/default.nix
@@ -1,10 +1,14 @@
 {
   cfg.zrepl-admin.homeManager =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     let
       generate-zrepl-ssl = pkgs.writeShellScriptBin "generate-zrepl-ssl" ''
         name=$1
-        ${pkgs.openssl}/bin/openssl req -x509 -sha256 -nodes -newkey rsa:4096 -days 365 -keyout $name.key -out $name.crt -addext "subjectAltName = DNS:$name" -subj "/CN=$name"
+        ${lib.getExe pkgs.openssl} req -x509 -sha256 -nodes -newkey rsa:4096 -days 365 -keyout $name.key -out $name.crt -addext "subjectAltName = DNS:$name" -subj "/CN=$name"
+        ${lib.getExe pkgs.sops} set modules/aspects/hosts/$name/secrets.yaml '["zrepl"]' "\"$(awk '{printf "%s\\n", $0}' $name.key)\""
+        rm $name.key
+
+        mv $name.crt files/certs/zrepl/
       '';
     in
     {
