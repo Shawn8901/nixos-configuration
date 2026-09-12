9e8317ba676f3ce6d6458345eea67b6fd0fdeba9
Shawn8901 <shawn8901@googlemail.com>
2026-09-12 21:28:09 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'firefox-addons':
        'gitlab:rycee/nur-expressions/fcf6f00c8e0869c68635cb624757f5824ad38e41?dir=pkgs/firefox-addons&narHash=sha256-dAGoOmXjG%2B5uPwTWgsIep1Qo6pPSsTG1feXRpC29xGA%3D' (2026-09-11)
      → 'gitlab:rycee/nur-expressions/811170bb74b11f3b9489618ce77487cc58c79067?dir=pkgs/firefox-addons&narHash=sha256-jaWL9beN3ifjNzNEBxo0abA%2BTSi0n7Q8LRQvZTxEqaM%3D' (2026-09-12)
    • Updated input 'flake-file':
        'github:denful/flake-file/c460df7fe65ac759989daad04f303fb427406e56?narHash=sha256-j4yjVCCguq6s/SIZ2eh2Ll5Z9cJh7VOkIIFNo2PXRd0%3D' (2026-08-31)
      → 'github:denful/flake-file/eccac77d2c2567efb6f368330930f3d55af8668a?narHash=sha256-o/o3n8%2BHAzFOFH83SuGW/w5J6glHtV%2Br8v%2BgJbMeoXM%3D' (2026-09-11)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/d0fd95e15e0c3baf565a9fd4bc352321e2564ea3?narHash=sha256-82D7ADWE8erhfALj3dqxAOeQaoa%2BBwlsdcp9v2m/ExM%3D' (2026-09-11)
      → 'github:nix-community/home-manager/fe48612c5321a2cc09b0f563757a9c9733d641f8?narHash=sha256-6hZ6Gy8isRQsWdI4l81Egag/PUIh68nSmFr2maInUNg%3D' (2026-09-12)

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# DO-NOT-EDIT. This file was auto-generated using github:denful/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
@@ -33,7 +33,7 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
-    import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:denful/import-tree";
     mimir = {
       url = "github:Shawn8901/mimir";
       inputs.nixpkgs.follows = "nixpkgs";
