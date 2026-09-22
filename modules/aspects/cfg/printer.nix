a3117e9091b773a5eaf188d3110256db4d7f9da4
Shawn8901 <shawn8901@googlemail.com>
2026-09-22 21:41:48 +0200

    desktop/printer: re-enable epson print driver

--- modules/aspects/cfg/printer.nix
+++ modules/aspects/cfg/printer.nix
@@ -23,7 +23,20 @@
           enable = true;
           browsed.enable = false;
           listenAddresses = [ "localhost:631" ];
-          # drivers = [ pkgs.epson-escpr2 ];
+          drivers = [ pkgs.epson-escpr2 ];
+        };
+
+        hardware.printers = {
+          ensureDefaultPrinter = "ET-3750";
+          ensurePrinters = [
+            {
+              name = "ET-3750";
+              location = "Office";
+              description = "Epson ET-3750";
+              deviceUri = "https://drucker:631/ipp/print";
+              model = "epson-inkjet-printer-escpr2/Epson-ET-3750_Series-epson-escpr2-en.ppd";
+            }
+          ];
         };
 
         environment.systemPackages = with pkgs.kdePackages; [
